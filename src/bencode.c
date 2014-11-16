8a8333735ba859757de076ac050d3fa6cbe186d1
Perevertun V.M. <vitalik.perevertun@gmail.com>
2014-11-16 23:05:25 +0200

    * bencode: read string from char buffer;

--- src/bencode.c
+++ src/bencode.c
@@ -16,25 +16,36 @@ int bc_decode_type(char c)
 	return BC_STRING;			// string
 }
 
-int bc_read_string(FILE *in, char **str)
+int bc_read_string(char *in, char **str)
 {
-	uint32_t size;
+	uint32_t size = 0;
 
-	if (fscanf(in, "%d:", &size) != 1)
-	     return -1;
+	size_t i = 0;
+	while (in[i] != ':')
+	{
+		size *= 10;
+		if (in[i] < '0' || in[i] > '9')
+			return 0;
+
+		size += in[i] - '0';
+		i++;
+	}
+	i++;
+
 	if (size < 1)
-	     return -2;
+		return 0;
 
 	char *p = malloc(size+1);
 	if (!p)
-		return -3;
+		return 0;
 
-	fread(p, 1, size, in);
+	memcpy(p, in + i, size);
+	i += size;
 	p[size] = '\0';
 
 	*str = p;
 
-	return 0;
+	return i+1;
 }
 
 int bc_read_int(FILE *in, int *val)
